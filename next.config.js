f95db760b79b093e635ea335c39dc1822a670447
zemse <sohamzemse143@gmail.com>
2019-04-29 12:46:59 +0530

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// jshint esversion:6
+const BabiliPlugin = require("babili-webpack-plugin");
+
+module.exports = {
+  webpack(config, { dev }) {
+    // remove Uglify plugin
+    config.plugins = config.plugins.filter(plugin => {
+      return plugin.constructor.name !== "UglifyJsPlugin";
+    });
+
+    if (!dev) {
+      // add Babili plugin
+      config.plugins.push(
+        new BabiliPlugin()
+      );
+    }
+
+    return config;
+  },
+};
